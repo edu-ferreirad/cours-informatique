0129fc7c9ceed242effa1c017d6c5aa95d02a20e
edu-ferreirad <79511395+edu-ferreirad@users.noreply.github.com>
2026-08-15 12:31:03 +0200

    Update jeu.js

--- PD/jeu_parent/jeu.js
+++ PD/jeu_parent/jeu.js
@@ -23,15 +23,15 @@ function setBackground(url) {
 // Fins négatives
 function scene_M1(){
     setBackground("perdu1.jpg");
-    window.alert("Afin d'éviter tout conflit, ne prenez pas d'initiative dans le dos de votre enfant. Privilégiez toujours le dialogue. Pour plus de conseils, référez-vous à la brochure !\n\nPour donner votre avis, veuillez scanner le QR code ci-dessous:");
+    window.alert("Afin d'éviter tout conflit, ne prenez pas d'initiative dans le dos de votre enfant. Privilégiez toujours le dialogue. Pour plus de conseils, référez-vous à la brochure !\n\n);
 }
 function scene_M2(){
     setBackground("perdu2.jpg");
-    window.alert("Il ne s'agit ici pas d'une erreur, surtout si vous n'avez pas les moyens de payer un abonnement. Cependant, votre adolescent arrivera toujours à trouver une connexion (Wifi de la ville, partage d'un ami, ...). Avec un abonnement, vous pourrez donc au moins garder un certain contrôle. Afin d'avoir plus de conseils, référez-vous à la brochure !\n\nPour donner votre avis, veuillez scanner le QR code ci-dessous:");
+    window.alert("Il ne s'agit ici pas d'une erreur, surtout si vous n'avez pas les moyens de payer un abonnement. Cependant, votre adolescent arrivera toujours à trouver une connexion (Wifi de la ville, partage d'un ami, ...). Avec un abonnement, vous pourrez donc au moins garder un certain contrôle. Afin d'avoir plus de conseils, référez-vous à la brochure !\n\n);
 }
 function scene_M3(){
     setBackground("perdu3.jpg");
-    window.alert("N'ayant pas anticipé, vous avez laissé le hasard décider à votre place, ce qui comporte certains risques (harcèlement, addiction, ...). Afin de pouvoir anticiper un éventuel problème, référez-vous à la brochure !\n\nPour donner votre avis, veuillez scanner le QR code ci-dessous:");
+    window.alert("N'ayant pas anticipé, vous avez laissé le hasard décider à votre place, ce qui comporte certains risques (harcèlement, addiction, ...). Afin de pouvoir anticiper un éventuel problème, référez-vous à la brochure !\n\n);
 }
 
 // Scène du début
@@ -86,7 +86,7 @@ function scene_3(){
 function scene_4(){
     setBackground("victoire_1.jpg");
     setTimeout(function(){
-        window.alert("Bien que vous ayez laissé le hasard intervenir, vous avez su prendre les bonnes décisions, au bon moment. Anticipez, dialoguez et n'oubliez pas que vous êtes le modèle. Pour plus de conseils, référez-vous à la brochure !\n\nPour donner votre avis, veuillez scanner le QR code ci-dessous:");
+        window.alert("Bien que vous ayez laissé le hasard intervenir, vous avez su prendre les bonnes décisions, au bon moment. Anticipez, dialoguez et n'oubliez pas que vous êtes le modèle. Pour plus de conseils, référez-vous à la brochure !\n\n);
         setBackground("the_end.jpg");
     }, 3000);
 }
@@ -110,21 +110,21 @@ function scene_5(){
 function scene_cachot(){
     setBackground("perdu4.jpg"); 
     setTimeout(function(){
-        window.alert("Le dialogue est fondamental pour accompagner votre enfant : s'il ne peut pas se confier à vous, il risque de le faire sur les réseaux... Afin d'avoir plus de conseils, référez-vous à la brochure !\n\nPour donner votre avis, veuillez scanner le QR code ci-dessous:");
+        window.alert("Le dialogue est fondamental pour accompagner votre enfant : s'il ne peut pas se confier à vous, il risque de le faire sur les réseaux... Afin d'avoir plus de conseils, référez-vous à la brochure !\n\n);
     }, 3000);
 }
 
 function scene_cachot1(){
     setBackground("perdu5.jpg"); 
     setTimeout(function(){
-        window.alert("Bien que le dialogue, fondamental pour accompagner votre enfant, ait été entamé, votre fatigue a fait que vous vous êtes emporté. La communication étant rompue, votre enfant pourrait ne plus se confier à vous et le faire sur les réseaux. Votre intention était bonne, mais lorsque vous souhaitez instaurer un dialogue, prenez bien votre temps. Afin d'avoir plus de conseils, référez-vous à la brochure !\n\nPour donner votre avis, veuillez scanner le QR code ci-dessous:");
+        window.alert("Bien que le dialogue, fondamental pour accompagner votre enfant, ait été entamé, votre fatigue a fait que vous vous êtes emporté. La communication étant rompue, votre enfant pourrait ne plus se confier à vous et le faire sur les réseaux. Votre intention était bonne, mais lorsque vous souhaitez instaurer un dialogue, prenez bien votre temps. Afin d'avoir plus de conseils, référez-vous à la brochure !\n\n);
     }, 3000);
 }
 
 function scene_7(){
     setBackground("victoire_2.jpg"); 
     setTimeout(function(){
-        window.alert("Bravo, malgré la fatigue, votre capacité de dialogue a permis de désamorcer la situation tendue. Anticipez, dialoguez et n'oubliez pas que vous êtes le modèle. Pour plus de conseils, référez-vous à la brochure !\n\nPour donner votre avis, veuillez scanner le QR code ci-dessous:"); 
+        window.alert("Bravo, malgré la fatigue, votre capacité de dialogue a permis de désamorcer la situation tendue. Anticipez, dialoguez et n'oubliez pas que vous êtes le modèle. Pour plus de conseils, référez-vous à la brochure !\n\n); 
     }, 3000);
 }
 
